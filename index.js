9a1b630fdd4fa89921b604862d233f6a8cf72dc9
Evaristo <bubbali2002@gmail.com>
2021-07-21 11:08:02 -0500

    commit tree 5108d6b8e5a5d673e12dc3a66c6cecbb9bec5543

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (number = 2000, number2 = 100) {
+  return number / number2;
+};
+
+const square = (parameter = 2) => {
+  return parameter * parameter;
+};
+
+const add = (parameter1=5, parameter2=2) =>{
+return parameter1 + parameter2 
+}
